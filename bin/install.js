c4231e6667beb1a1373733b417e84b36cd16c4c4
liyongkai <liyongkai@xiaoyouzi.com>
2017-01-06 11:20:42 +0800

    test

--- bin/install.js
+++ bin/install.js
@@ -1,17 +1,21 @@
 // Run when package is installed
-var fs = require('fs')
-var path = require('path')
-var isCI = require('is-ci')
-var husky = require('husky')
+// var fs = require('fs')
+// var path = require('path')
+// var isCI = require('is-ci')
+// var husky = require('husky')
 
-if (isCI) {
-  console.log('\033[4;36m%s\033[0m', 'husky')
-  console.log('CI detected, skipping Git hooks installation')
-  return
-}
+// if (isCI) {
+//   console.log('\033[4;36m%s\033[0m', 'husky')
+//   console.log('CI detected, skipping Git hooks installation')
+//   return
+// }
 
-console.log('\033[4;36m%s\033[0m', 'husky')
-console.log('setting up hooks')
+// console.log('\033[4;36m%s\033[0m', 'husky')
+// console.log('setting up hooks')
 
-var huskyDir = path.join(__dirname, '..')
-husky.installFrom(huskyDir)
+// var huskyDir = path.join(__dirname, '..')
+// husky.installFrom(huskyDir)
+
+
+console.log('===== __dirname: ' + __dirname + '=====');
+console.log('=====__filename: ' + __filename + '=====');
--- bin/uninstall.js
+++ bin/uninstall.js
@@ -1,9 +1,9 @@
 // Run when package is uninstalled
-var path = require('path')
-var husky = require('husky')
+// var path = require('path')
+// var husky = require('husky')
 
-console.log('\033[4;36m%s\033[0m', 'husky')
-console.log('uninstalling')
+// console.log('\033[4;36m%s\033[0m', 'husky')
+// console.log('uninstalling')
 
-var huskyDir = path.join(__dirname, '..')
-husky.uninstallFrom(huskyDir)
+// var huskyDir = path.join(__dirname, '..')
+// husky.uninstallFrom(huskyDir)
